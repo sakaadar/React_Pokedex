db69330e3e1931af4a4b5d0f7690aab7ecb5b370
Sakaadar <79919443+Sakaadar@users.noreply.github.com>
2025-04-28 05:04:11 +0200

    Latest edition .....

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { HashRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
 import PokemonDetail from './Components/PokemonDetail.jsx';
 import About from './Components/About.jsx';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import React from 'react';
+import './App.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename="/React_Pokedex/">
+  <HashRouter>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="/pokemon/:id" element={<PokemonDetail />} />
       <Route path="/about" element={<About />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
